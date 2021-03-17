df587588ce7d959f7e5b48292cfe11b80a17e759
Derfies <Derfies@users.noreply.github.com>
2021-03-17 22:26:48 +1100

    [EDIT] - test_clingo.py - Wrapper for networkx graphs. [ADD] - e1m5.gexf - *Kinda* the level flow for blood e1m5.

--- reactor/wip/test_clingo.py
+++ reactor/wip/test_clingo.py
@@ -1,80 +1,109 @@
+import abc
+import random
+
 import clyngor
 from clyngor import solve
 
 from reactor.readers.gexfreader import GEXFReader
 
 
-GRID_PATH = '../../data/reactor1.gexf'
+GRID_PATH = '../../data/graphs/blood/e1m5.gexf'
 
 
-def graph_to_asp(g):
+class AspLevelTheme(metaclass=abc.ABCMeta):
 
-    nodes_str = ' '.join([
-        f'node({node.lower()}).'
-        for node in g.nodes()
-    ])
+    def __init__(self, g):
+        self.g = g
 
-    edges_str = ' '.join([
-        f'edge({head.lower()}, {tail.lower()}).'
-        for head, tail in g.edges()
-    ])
+    @staticmethod
+    def format_fact(label, *args):
+        args_str = ', '.join(map(str, args))
+        return f'{label}({args_str}).'
 
-    #extra = 'num_nodes()'
+    @property
+    def num_nodes(self):
+        return len(self.g)
 
-    return nodes_str + ' ' + edges_str# + extra
+    @property
+    def node_facts(self):
+        return [
+            self.format_fact('node', node.lower())
+            for node in self.g
+        ]
 
-import random
+    @property
+    def edge_facts(self):
+        return [
+            self.format_fact('edge', head.lower(), tail.lower())
+            for head, tail in self.g.edges()
+        ]
+
+    @property
+    @abc.abstractmethod
+    def zone_facts(self):
+        """"""
+
+    @property
+    def facts(self):
+        facts = []
+        facts.extend(self.node_facts)
+        facts.extend(self.edge_facts)
+        facts.extend(self.zone_facts)
+        return facts
+
+    def get_clingo_kwargs(self):
+        return {
+            'constants': {
+                'num_nodes': self.num_nodes
+            },
+            'inline': '\n'.join(self.facts)
+        }
+
+
+class CastleTheme(AspLevelTheme):
+
+    @property
+    def zone_facts(self):
+        return [
+            self.format_fact('type_range', 'entrance', 1, 1),
+            self.format_fact('type_range', 'courtyard', 0, 1),
+            self.format_fact('type_range', 'cellar', 0, 1),
+            self.format_fact('type_range', 'chamber', 1, int(self.num_nodes / 3)),
+            self.format_fact('type_range', 'main_chamber', 1, 1),
+            self.format_fact('type_range', 'hallway', 0, 15),
+            self.format_fact('type_range', 'room', 0, 15),
+        ]
+
+
 # Load a map.
 g = GEXFReader()(GRID_PATH).to_undirected()
-asp_g = graph_to_asp(g)
+theme = CastleTheme(g)
 
+# Run solver.
+seed = random.randint(0, 999999999)
+print(seed)
 clyngor.CLINGO_BIN_PATH = '../../bin/clingo-5.4.0-win64/clingo.exe'
 answers = solve(
-    '../../data/clingo/test2.lp',
-    inline=asp_g,
+    '../../data/clingo/theme.lp',
     use_clingo_module=False,
     nb_model=1,
-    constants={'num_nodes': len(g)},
-    options=f'--seed={random.randint(0, 10000)} --rand-freq=1'# --sign-def=3'
+    options=f'--rand-freq=1 --seed={seed}',
+    delete_tempfile=False,
+    **theme.get_clingo_kwargs()
 )
+
 print(answers.command)
 
 answers = list(answers)
 print('num answers:', len(answers))
+
+print('Facts:')
+for f in theme.facts:
+    print('    ->', f)
+
 for a in answers:
     print('-' * 25)
     for node_type in sorted(a):
         print('->', node_type)
 if not len(answers):
     print('no answers')
-
-# Check
-num_nodes = len(g)
-type_range = {
-    'entrance': (1, 1),
-    'courtyard': (0, 1),
-    'chamber': (1, num_nodes / 3),
-    'main_chamber': (1, 1),
-    'hallway': (0, num_nodes)
-}
-print(type_range)
-
-high_chambers = set()
-for answer in answers:
-    for p in answer:
-        if p[0] != 'type_num':
-            continue
-        name, num = p[1]
-        #print(name, '->', num)
-        range = type_range[name]
-        assert range[0] <= num <= range[1]
-
-        if name == 'chamber' and num > 1:
-            high_chambers.add(answer)
-        # for k, v in type_range.items():
-        #     name, num = p[1]
-        #     print(v[name])
-        #     assert v[name][0] < num < v[name][1]
-
-
-print('num high chambers:', len(high_chambers))
